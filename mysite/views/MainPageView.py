12cb5aa547855fa153a65ce844f5bf47e4dfed41
Martyna Grzegorczyk <m.grzegorczyk@bonasoft.pl>
2022-01-01 20:11:13 +0100

    add templates

--- mysite/views/MainPageView.py
+++ mysite/views/MainPageView.py
@@ -1,21 +1,21 @@
 from django.http import HttpResponse
 from django.views import View
-from mysite.services.insert_objects_to_database import insert_points_to_db
+from django.shortcuts import render
 from mysite.models.point import Point
 
 
 class MyView(View):
     def get(self, request):
-        return HttpResponse('Nasz strona gówna')
+        return HttpResponse('Nasz strona')
 
 
 class SavePoints(View):
     def get(self, request):
-        insert_points_to_db()
-        return HttpResponse('saved')
+        point = Point.objects.all()[0]
+        return render(request, 'login.html', {'point': point})
 
 
 class GetPoints(View):
     def get(self, request):
-        queryset = list(Point.objects.all())
+        queryset = Point.objects.values_list('name')
         return HttpResponse(queryset)
