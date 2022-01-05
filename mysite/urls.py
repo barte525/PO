e33aba01b4390c31ce3e5b4bc3ea45871126fffb
barte525 <bartek5251@gmail.com>
2022-01-05 23:04:29 +0100

    develop login PU

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from mysite.views.MainPageView import MyView, SavePoints, GetPoints
 from django.contrib.auth import views as auth_views
+from mysite.views.profile import ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', MyView.as_view()),
     path('save/', SavePoints.as_view()),
     path('get/', GetPoints.as_view()),
-    path('login/', auth_views.LoginView.as_view(template_name="login.html"))
+    path('login/', auth_views.LoginView.as_view(template_name="login.html")),
+    path('profile/', ProfileView.as_view())
 ]
