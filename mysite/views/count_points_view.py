d0428284f2fe8c795866268b3b1c81aaf0edf8b7
Martyna Grzegorczyk <m.grzegorczyk@bonasoft.pl>
2022-01-07 15:12:24 +0100

    sth changed

--- mysite/views/count_points_view.py
+++ mysite/views/count_points_view.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.shortcuts import render
 from mysite.models.point import Point
 from mysite.models.range import Range
+from mysite.models.route import Route
+from mysite.models.user import User, Tourist
 from django.http import HttpResponse
 
 
@@ -10,7 +12,12 @@ class CountPoints(View):
         return render(request, 'countpoints.html')
 
     def post(self, request):
-        points =
+        user = User.objects.get(id=id)
+        tourist = Tourist.objects.get(user=user)
+        routes = Route.objects.get(tourist=tourist)
+        for route in routes:
+            points = 1
+
         message_positive = "Gratulujemy zaangażowania i życzymy powodzenia w dalszym zdobywaniu szczytów!"
         message_zero = "Cieszymy się, że pragniesz zdobyć odznakę. \n Wprowadzaj przebyte trasy i podliczaj zdobyte punkty. Do dzieła!"
 
